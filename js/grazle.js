2daec78c21be1f42ddcd5bb72f0e923386445773
Grey Lindley <grey@ghldigital.com>
2020-05-22 11:21:21 +0100

    Update to $getHeight

--- js/grazle.js
+++ js/grazle.js
@@ -360,13 +360,15 @@
 
 
 	export function $getHeight(el) {
-		const styles = window.getComputedStyle(el);
-		const height = el.offsetHeight;
-		const borderTopWidth = parseFloat(styles.borderTopWidth);
-		const borderBottomWidth = parseFloat(styles.borderBottomWidth);
-		const paddingTop = parseFloat(styles.paddingTop);
-		const paddingBottom = parseFloat(styles.paddingBottom);
-		return height - borderBottomWidth - borderTopWidth - paddingTop - paddingBottom;
+		if($isElementPresent(el)){
+			const styles = window.getComputedStyle(el);
+			const height = el.offsetHeight;
+			const borderTopWidth = parseFloat(styles.borderTopWidth);
+			const borderBottomWidth = parseFloat(styles.borderBottomWidth);
+			const paddingTop = parseFloat(styles.paddingTop);
+			const paddingBottom = parseFloat(styles.paddingBottom);
+			return height - borderBottomWidth - borderTopWidth - paddingTop - paddingBottom;
+		}
 	}
 
 
